a824f8d0f1bc2c4a33bcb9ee8aad9f465d07da16
arthurtheray <a.votalub@gmail.com>
2025-10-04 08:47:05 +0500

    prg 3 is done

---
+++ src/main/java/prg3/Main.java
@@ -0,0 +1,33 @@
+package prg3;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            try {
+                double celsiumInput = askForCelsiumValue();
+                System.out.printf("%.2f градусов цельсия это %.2f фаренгейт",
+                        askForCelsiumValue(), convertCelsiumToFahrenheit(celsiumInput));
+                break;
+            } catch (IOException e) {
+                System.out.println("Не удалось обработать ввод. Введи еще раз");
+            }
+        }
+    }
+
+    public static double askForCelsiumValue() throws IOException {
+        System.out.println("Введите число в цельсиях:");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            return Double.parseDouble(scanner.nextLine());
+        } catch (Exception e) {
+            throw new IOException("Input not parsable to double ", e.getCause());
+        }
+    }
+
+    public static double convertCelsiumToFahrenheit(double celsiumValue) {
+        return Math.round(celsiumValue * 1.8 + 32);
+    }
+}
