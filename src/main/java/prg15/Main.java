3f8e2ba07fe8111d6a623253de22bb13b5d67467
arthurtheray <a.votalub@gmail.com>
2025-10-13 23:58:15 +0500

    prg15 done

--- src/main/java/prg15/Main.java
+++ src/main/java/prg15/Main.java
@@ -10,38 +10,32 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] firstSortedArray = {0, 1, 3, 123, 123, 1111, 2222, 1555555, 123344664};
-        int[] secondSortedArray = {15, 124, 1111, 22225, 1555355, 123344664, 1233213213};
-        int[] collapsedArray = collapseArrays(firstSortedArray, secondSortedArray);
+        int[] secondSortedArray = {-15, 124, 1111, 22225, 1555355, 123344664, 1233213213, 1233213216, 1233213244, 1233213266, 1233213277, 1233213999};
+        int[] collapsedArray = collapseArrays(secondSortedArray, firstSortedArray);
         System.out.println(Arrays.toString(collapsedArray));
+        int[] collapsedArray2 = collapseArrays(firstSortedArray, secondSortedArray);
+        System.out.println(Arrays.toString(collapsedArray2));
     }
 
     private static int[] collapseArrays(int[] firstSortedArray, int[] secondSortedArray) {
         int[] collapsedArray = new int[firstSortedArray.length + secondSortedArray.length];
-        int i = 0;
-        int j = 0;
-        while (i < firstSortedArray.length) {
-            if (firstSortedArray[i] <= secondSortedArray[i]) {
-                collapsedArray[j] = firstSortedArray[i];
-                j++;
-                collapsedArray[j] = secondSortedArray[i];
-                j++;
-            } else {
-                collapsedArray[j] = secondSortedArray[i];
-                j++;
-                collapsedArray[j] = firstSortedArray[i];
-                j++;
+        int firstIndex = 0;
+        int secondIndex = 0;
+        for (int i = 0; i <= firstSortedArray.length + secondSortedArray.length; i++) {
+            if (firstIndex != firstSortedArray.length && firstSortedArray[firstIndex] <= secondSortedArray[secondIndex]) {
+                collapsedArray[i] = firstSortedArray[firstIndex];
+                firstIndex++;
+                continue;
             }
-            i++;
-        }
-        while (j < firstSortedArray.length + secondSortedArray.length) {
-            if (firstSortedArray.length <= secondSortedArray.length) {
-                collapsedArray[j] = secondSortedArray[i];
-                i++;
-                j++;
-            } else {
-                collapsedArray[j] = firstSortedArray[i];
-                i++;
-                j++;
+            collapsedArray[i] = secondSortedArray[secondIndex];
+            secondIndex++;
+            if (secondIndex == secondSortedArray.length) {
+                while (i != collapsedArray.length - 1) {
+                    i++;
+                    collapsedArray[i] = firstSortedArray[firstIndex];
+                    firstIndex++;
+                }
+                return collapsedArray;
             }
         }
         return collapsedArray;
